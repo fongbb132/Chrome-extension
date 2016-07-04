a74cf5520a3a30cd12c2807805c3ddca9f72f653
fongbb132 <fo_og@hotmail.com>
2016-07-04 00:56:42 -0400

    link selector is able to move forward and backward and enter a page

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // document.body.style.backgroundColor="grey";
 
 
+var currentLink=null;
+var linkList = [];
+var index = 0;
+var currentPos=-1;
 function findLinks(){	
 	var isSet = false;
 
@@ -8,27 +12,51 @@ function findLinks(){
 	var left = (window.pageXOffset || doc.scrollLeft) - (doc.clientLeft || 0);
 	var top = (window.pageYOffset || doc.scrollLeft) - (doc.clientTop || 0);
 	
+	var isSet = 0;
 	$("a").each(function(){
 		var link = $(this);
+		if( isSet < 1 && link.offset().top > top && link.offset().left > left){
+		
+			currentLink = link;
 
-		if(!isSet && link.offset().top > top && link.offset().left > left){
-			link.css('border',  'thin dashed grey');	
-			isSet = true;
+			currentLink.addClass('current');
+	  		currentPos = index;
+			isSet++;
+
 		}
-
+		index++;
+		linkList.push(link);
 	});
 	
+    // currentLink.addClass('current');
 }
 
 
-
-//z: 90		f: 70	b:66	enter:13
-var map = {90: false, 70: false, 66: false, 13:false};
+//z: 90		f: 70	b:66	enter:13	i:73 	j:74	k:75 	l:76
+var map = {90: false, 70: false, 66: false, 13:false, 73:false, 74: false, 75:false, 76:false};
 $(document).keydown(function(e) {
     if (e.keyCode in map) {
         map[e.keyCode] = true;
-        if (map[90] && map[13]) {
-            findLinks();
+        if (map[90] && map[76]) {
+        	if(currentPos<0){
+        	    findLinks();
+        	}else{
+        		currentLink.removeClass('current');
+        		currentLink = linkList[++currentPos];
+        		currentLink.addClass('current');
+        	}
+        }
+        else if(map[90] && map[74]){
+        	if(currentPos<0){
+        	    findLinks();
+        	}else{
+        		currentLink.removeClass('current');
+        		currentLink = linkList[--currentPos];
+        		currentLink.addClass('current');
+        	}
+        }
+        else if(map[90] && map[13]){
+        	window.location = currentLink.attr("href") ;
         }
     }
 }).keyup(function(e) {
